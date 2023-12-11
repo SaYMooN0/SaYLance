ba1145d95bab1f35c2f999ac9bf4c15acd6894d4
SaYMooN <shokoladnysaymoon@mail.ru>
2023-12-11 21:27:35 +0300

    Добавьте файлы проекта.

---
+++ SaYLance/Interpreter.cs
@@ -0,0 +1,10 @@
+﻿namespace SaYLance
+{
+    public static class Interpreter
+    {
+        public static int Run(string filePath)
+        {
+            return 0;
+        }
+    }
+}
---
+++ SaYLanceDE/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using SaYLance;
+using System.Windows;
+namespace SaYLanceDE
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Interpreter.Run("str");
+        }
+    }
+}
